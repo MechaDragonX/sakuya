4b599be8bc5d6d2b2bbbeb141730f819f60cf392
MechaDragonX <theofficialrv@gmail.com>
2020-03-21 18:01:19 -0700

    Pong back a message using

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as Discord from 'discord.js';
-const config = require('../config.json');
+const { prefix, token } = require('../config.json');
 
 const client: any = new Discord.Client();
 
@@ -7,10 +7,13 @@ client.once('ready', (): any => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
-	if(message.content === '!ping') {
-        message.channel.send('Pong!');
+client.on('message', (message: any) => {
+	if(message.content.startsWith(`${prefix}ping`)) {
+        if(message.content[5] != ' ')
+            message.channel.send('You need to have a space after `!ping`!');
+        else
+            message.channel.send(message.content.slice(6));
     }
 });
 
-client.login(config.token);
+client.login(token);
