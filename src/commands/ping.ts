45298218e4adce92e7a134662f557bb66970993d
MechaDragonX <theofficialrv@gmail.com>
2020-03-21 19:51:23 -0700

    Seperate ping command into separate file

---
+++ src/commands/ping.ts
@@ -0,0 +1,12 @@
+import * as Discord from 'discord.js';
+
+module.exports = {
+	name: 'ping',
+	description: 'Ping back a message!',
+	execute(message: Discord.Message, args: string[]) {
+		if(!args.length) {
+            return message.reply('Pong!');
+        }
+        return message.channel.send(args.join(' '));
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as Discord from 'discord.js';
 const { prefix, token } = require('../config.json');
+const ping = require('./commands/ping');
 
 const client: any = new Discord.Client();
 
@@ -7,24 +8,15 @@ client.once('ready', (): any => {
 	console.log('Ready!');
 });
 
-client.on('message', (message: any) => {
+client.on('message', (message: Discord.Message) => {
     if(!message.content.startsWith(prefix) || message.author.bot)
         return;
     
     const args: string[] = message.content.slice(prefix.length).split(/ +/);
     const command: string = args.shift().toLowerCase();
 
-    if(command === 'args-info') {
-        if(!args.length) {
-            return message.reply(`You didn't provide any arguments, ${message.author.username}!`);
-        }
-        return message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    }
     if(command === 'ping') {
-        if(!args.length) {
-            return message.reply('Pong!');
-        }
-        return message.channel.send(args.join(' '));
+        ping.execute(message, args);
     }
 });
 
